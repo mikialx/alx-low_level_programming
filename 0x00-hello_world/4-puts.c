977a99c00fa1f081748792ac37fe1d2ece05cb8e
mikialx <michaelkifle2039@gmail.com>
2021-08-28 14:46:51 -0700

    my c cod

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - prints string
  * descriptio:prins "\"programming is like building a multilingual puzzle"
 *RETURN:0
 */
+
 int main(void)
 {
   puts("\"Programmin is like building a multilingual puzzle");
