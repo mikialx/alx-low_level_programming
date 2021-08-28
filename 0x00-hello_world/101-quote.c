560bb444cadaa0fadb451df52910abf5e21f6209
mikialx <89109765+mikialx@users.noreply.github.com>
2021-08-28 15:24:08 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,21 +2,20 @@
 /**
  * main - outputs to string
  *
- *Decription: prints out "and that piece of art is useful ..." without printf
+ * Description: Prints out "and that piece of art is useful..." without printf
  *
-Return:1
-*/
-
+ * Return: 1
+ */
 int main(void)
 {
-  char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 long l = 59;
 long fd = 1;
 long syscall = 1;
 long ret = 0;
-__asm__("syscall"
-: "a" (ret)
-: "a" (sycall),
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
 "D" (fd),
 "S" (s),
 "d" (l));
