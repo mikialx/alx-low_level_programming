47a657070c51d1e02f026bb9da8facb03f9a6220
mikialx <89109765+mikialx@users.noreply.github.com>
2021-08-28 14:52:47 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - prints string
- * descriptio:prins "\"programming is like building a multilingual puzzle"
-*RETURN:0
-*/
-
+ * main - Prints string
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
 {
-  puts("\"Programmin is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
