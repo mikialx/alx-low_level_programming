61f19fd96731dd128f99364f9a5e70f02a0cd42f
mikialx <michaelkifle2039@gmail.com>
2021-08-31 10:47:31 -0700

    my loop

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code for ALX School students.
- *
+ * print_last_digit - prints the last digit of a number
+ *@n: number being tested
  * Return: Always 0.
  */
-int main(void)
-{
-	int r;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+		n = -n;
+		_putchar(n + '0');
+		return (n);
 }
