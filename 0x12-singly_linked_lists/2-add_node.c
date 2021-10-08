a9f4e038228608d76a6e3554dbade455438181c6
mikialx <michaelkifle2039@gmail.com>
2021-10-08 15:10:42 +0300

    edited

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,44 +1,69 @@
-#include <string.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
-  *_strlen - finds length of a string.
-  *@str: string.
-  *
-  *Return: lengt of string.
-  */
-int _strlen(const char *str)
+ * _strlen - finds the length of as string passed to it
+ * @s: string whose length is to be found
+ * Return: length of the string
+ */
+int _strlen(const char *s)
 {
-	int i;
+	int x = 0;
 
-	while (str[i] != '\0')
-		i++;
-
-	return (i);
+	while (*(s + x) != '\0')
+		x++;
+	return (x);
 }
 
 /**
-  *add_node - adds a new node at the beginning of a list_t.
-  *@head: pointer to first element of the list.
-  *@str: string to be duplicated.
-  *
-  *Return: address of the new element or NULL if it failed.
-  */
+ * _strcpy - copies a string pointed to by src to buffer pointed to by dest,
+ * including the terminating null byte '\0'
+ * @dest: pointer pointing to buffer to which string will be copied
+ * @src: pointer pointing to string to be copies
+ * Return: pointer pointing to buffer containing copied string
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int x, y;
+
+	x = 0;
+	while (*(src + x) != '\0')
+		x++;
+	for (y = 0; y <= x; y++)
+		*(dest + y) = *(src + y);
+	return (dest);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list.
+ * @head: double pointer to head of the linked list lits_t
+ * @str: string to initialize list_t's str variable with
+ *
+ * Return: Address of new element of NULL if it failed
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *temp;
+	list_t *new;
 
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
+	new = (list_t *)malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(new);
 		return (NULL);
-	/* temp now points to first element*/
-	temp->next = *head;
-	temp->str = strdup(str);
-	temp->len = _strlen(str);
-
-	*head = temp;
-
+	}
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new->str = malloc(sizeof(char) * _strlen(str));
+	if (new->str == NULL)
+	{
+		free(new->str);
+		free(new);
+		return (NULL);
+	}
+	new->str = _strcpy(new->str, str);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
 	return (*head);
-
 }
