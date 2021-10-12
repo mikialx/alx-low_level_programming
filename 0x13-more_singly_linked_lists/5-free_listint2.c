cf005adc356cc7838b3c65779a7eb9ff5ac0686b
mikialx <michaelkifle2039@gmail.com>
2021-10-12 15:20:29 +0300

    new update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - main function
+ * @head: The address of pointer to first node.
+ *
+ * Description: This function frees a listint_t list (2).
+ *
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *list;
+
+	if (!head)
+	{
+		return;
+	}
+
+	while (*head)
+	{
+		list = *head;
+		*head = list->next;
+		free(list);
+	
